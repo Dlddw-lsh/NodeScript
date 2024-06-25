d1fcac0a65f43f9542b844bdf235da712757be3c
lsh <3040114965@qq.com>
2024-06-25 12:07:12 +0800

    rmTable.sh

---
+++ rmTable.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+sudo -u postgres psql -c "drop table brc20_block_hashes;"
+sudo -u postgres psql -c "drop table brc20_cumulative_event_hashes;"
+sudo -u postgres psql -c "drop table brc20_current_balances;"
+sudo -u postgres psql -c "drop table brc20_event_types;"
+sudo -u postgres psql -c "drop table brc20_events;"
+sudo -u postgres psql -c "drop table brc20_extras_block_hashes;"
+sudo -u postgres psql -c "drop table brc20_historic_balances;"
+sudo -u postgres psql -c "drop table brc20_indexer_version;"
+sudo -u postgres psql -c "drop table brc20_tickers;"
+sudo -u postgres psql -c "drop table brc20_unused_tx_inscrs;"
